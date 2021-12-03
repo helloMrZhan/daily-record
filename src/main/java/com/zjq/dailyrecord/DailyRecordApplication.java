7a2edfd5118f2e8dad7799e5c819b39861a6b284
Jack <1187625098@qq.com>
2021-12-03 21:32:14 +0800

    日常代码记录初始化

---
+++ src/main/java/com/zjq/dailyrecord/DailyRecordApplication.java
@@ -0,0 +1,17 @@
+package com.zjq.dailyrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 日常记录
+ * @author zjq
+ */
+@SpringBootApplication
+public class DailyRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjq/dailyrecord/DailyRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjq.dailyrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
