90453b133cec013dd7cb3c9ef9a7477ffbcedcdc
Jack <1187625098@qq.com>
2022-03-20 22:42:13 +0800

    '工厂模式'

---
+++ src/main/java/com/zjq/dailyrecord/designpattern/spring/factory/UserService.java
@@ -0,0 +1,10 @@
+package com.zjq.dailyrecord.designpattern.spring.factory;
+
+/**
+ * @author zjq
+ * @date 2022/3/20 16:04
+ * <p>title:</p>
+ * <p>description:</p>
+ */
+public interface UserService {
+}
