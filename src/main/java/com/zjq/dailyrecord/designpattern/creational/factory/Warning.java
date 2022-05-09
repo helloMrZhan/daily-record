5cd7156a332f9e9d46e490083a1a482da2898be4
zhanjq <1187625098@qq.com>
2022-05-09 12:56:00 +0800

    警告接口

---
+++ src/main/java/com/zjq/dailyrecord/designpattern/creational/factory/Warning.java
@@ -0,0 +1,13 @@
+package com.zjq.dailyrecord.designpattern.creational.factory;
+
+/**
+ * 告警接口
+ * @author zjq
+ * @date 2022/5/9
+ */
+public interface Warning {
+    /**
+     * 发送告警信息
+     */
+    void sendWarnMSG();
+}
