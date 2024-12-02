cdaca8d0d42c4e882b21f93292790fb668ccd159
zjq <1187625098@qq.com>
2024-12-02 14:34:07 +0800

    feat：AES加密解密工具类

---
+++ src/main/java/com/zjq/dailyrecord/security/AESEncryptionDecryption.java
@@ -0,0 +1,72 @@
+package com.zjq.dailyrecord.security;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * AES加密解密工具类
+ * @author zjq
+ */
+public class AESEncryptionDecryption {
+
+    public static String decrypt(String encryptedData, String key, String iv) throws Exception {
+        // 将密钥和IV从字符串转换为字节数组
+        byte[] keyBytes = key.getBytes("UTF-8");
+        byte[] ivBytes = iv.getBytes("UTF-8");
+
+        // 创建SecretKeySpec和IvParameterSpec对象
+        SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+
+        // 创建并初始化Cipher对象
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
+
+        // 对加密数据进行Base64解码
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedData);
+
+        // 执行解密操作
+        byte[] originalBytes = cipher.doFinal(encryptedBytes);
+
+        // 将解密后的字节数组转换为字符串
+        return new String(originalBytes, "UTF-8");
+    }
+
+    public static String encrypt(String plainText, String key, String iv) throws Exception {
+        // 将密钥和IV从字符串转换为字节数组
+        byte[] keyBytes = key.getBytes("UTF-8");
+        byte[] ivBytes = iv.getBytes("UTF-8");
+
+        // 创建SecretKeySpec和IvParameterSpec对象
+        SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+
+        // 创建并初始化Cipher对象
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivSpec);
+
+        // 执行加密操作
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes("UTF-8"));
+
+        // 对加密数据进行Base64编码
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String plainText = "这是要加密的文本";
+            String key = "1234567890123456"; // 16字节的密钥
+            String iv = "1234567890123456"; // 16字节的IV
+
+            String encryptedData = encrypt(plainText, key, iv);
+            System.out.println("加密后的数据: " + encryptedData);
+
+            String decryptedData = decrypt(encryptedData, key, iv);
+            System.out.println("解密后的数据: " + decryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
