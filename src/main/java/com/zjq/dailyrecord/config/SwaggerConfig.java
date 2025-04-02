4b0b440735108a6e7c9214ad7af15c2b17441fcd
zjq <1187625098@qq.com>
2025-04-02 14:23:49 +0800

    feat：添加knife4j（集Swagger2 和 OpenAPI3 为一体的增强解决方案）配置

---
+++ src/main/java/com/zjq/dailyrecord/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.zjq.dailyrecord.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * Swagger配置类
+ * @author 共饮一杯无
+ */
+//指定为配置类
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfig {
+    /**
+     * 添加摘要信息(Docket)
+     */
+    @Bean
+    public Docket controllerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("API文档标题")
+                        .description("文档描述：用于实时查看、测试API")
+                        .contact(new Contact("共饮一杯无", "https://zhanjq.blog.csdn.net/", ""))
+                        .version("版本号:1.0")
+                        .build())
+                .select()
+                //API基础扫描路径
+                .apis(RequestHandlerSelectors.basePackage("com.zjq.dailyrecord"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
